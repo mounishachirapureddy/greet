9481e333def9dc2f75ef1a4ba348082868d7c9ab
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-12-11 22:06:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 
 # Create VPC
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "messaging_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -17,92 +17,92 @@ resource "aws_vpc" "my_vpc" {
 
 
 # Create Subnets
-resource "aws_subnet" "subnet_a" {
+resource "aws_subnet" "messaging_subnet_a" {
 depends_on = [
     aws_vpc.my_vpc
   ]
   
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.messaging_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "subnet-a"
+    Name = "messaging_subnet-a"
   }
 }
 
-resource "aws_subnet" "subnet_b" {
+resource "aws_subnet" "messaging_subnet_b" {
 depends_on = [
-    aws_vpc.my_vpc
+    aws_vpc.messaging_vpc
   ]
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.messaging_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name = "subnet-b"
+    Name = "messaging_subnet-b"
   }
 }
 
 
 
 # Create Private Subnets
-resource "aws_subnet" "private_subnet_a" {
+resource "aws_subnet" "messaging_private_subnet_a" {
 depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_a
+    aws_vpc.messaging_vpc,
+    aws_subnet.messaging_subnet_a
   ]
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.messaging_vpc.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "ap-south-1a"
   tags = {
-    Name = "private-subnet-a"
+    Name = "messaging_private-subnet-a"
   }
 }
 
-resource "aws_subnet" "private_subnet_b" {
+resource "aws_subnet" "messaging_private_subnet_b" {
 depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_b
+    aws_vpc.messaging_vpc,
+    aws_subnet.messaging_subnet_b
   ]
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.messaging_vpc.id
   cidr_block              = "10.0.4.0/24"
   availability_zone       = "ap-south-1b"
   tags = {
-    Name = "private-subnet-b"
+    Name = "messaging_private-subnet-b"
   }
 }
 
 
 # Create Internet Gateway
-resource "aws_internet_gateway" "my_igw" {
+resource "aws_internet_gateway" "messaging_igw" {
 depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_a,
-    aws_subnet.subnet_b
+    aws_vpc.messaging_vpc,
+    aws_subnet.messaging_subnet_a,
+    aws_subnet.messaging_subnet_b
   ]
 
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.messaging_vpc.id
   tags = {
-    Name = "my-igw"
+    Name = "messaging-igw"
 }
 }
 
 
 # Creating an Route Table for the public subnet
-resource "aws_route_table" "Public-Subnet-RT" {
+resource "aws_route_table" "messaging_Public-Subnet-RT" {
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_internet_gateway.my_igw
+    aws_vpc.messaging_vpc,
+    aws_internet_gateway.messaging_igw
   ]
 
    # VPC ID
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.messaging_vpc.id
 
   # NAT Rule
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.messaging_igw.id
   }
 
   tags = {
@@ -114,39 +114,39 @@ resource "aws_route_table" "Public-Subnet-RT" {
 
 
 # Creating a resource for the Route Table Association with subnet_a
-resource "aws_route_table_association" "RT-IG-Association-a" {
+resource "aws_route_table_association" "messaging_RT-IG-Association-a" {
 
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_a,
-    aws_subnet.subnet_b,
-    aws_route_table.Public-Subnet-RT
+    aws_vpc.messaging_vpc,
+    aws_subnet.messaging_subnet_a,
+    aws_subnet.messaging_subnet_b,
+    aws_route_table.messsaging_Public-Subnet-RT
   ]
 
 # Public Subnet ID
-  subnet_id      = aws_subnet.subnet_a.id
+  subnet_id      = aws_subnet.messaging_subnet_a.id
 
 #  Route Table ID
-  route_table_id = aws_route_table.Public-Subnet-RT.id
+  route_table_id = aws_route_table.messaging_Public-Subnet-RT.id
 }
 
 
 
 # Creating a resource for the Route Table Association with subnet_b
-resource "aws_route_table_association" "RT-IG-Association-b" {
+resource "aws_route_table_association" "messaging_RT-IG-Association-b" {
 
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_a,
-    aws_subnet.subnet_b,
-    aws_route_table.Public-Subnet-RT
+    aws_vpc.messaging_vpc,
+    aws_subnet.messaging_subnet_a,
+    aws_subnet.messaging_subnet_b,
+    aws_route_table.messaging_Public-Subnet-RT
   ]
 
 # Public Subnet ID
-  subnet_id      = aws_subnet.subnet_b.id
+  subnet_id      = aws_subnet.messaging_subnet_b.id
 
 #  Route Table ID
-  route_table_id = aws_route_table.Public-Subnet-RT.id
+  route_table_id = aws_route_table.messaging_Public-Subnet-RT.id
 }
 
 
@@ -155,7 +155,7 @@ resource "aws_route_table_association" "RT-IG-Association-b" {
 
 
 # Creating an Elastic IP for the NAT Gateway
-resource "aws_eip" "Nat-Gateway-EIP" {
+resource "aws_eip" "messaging_Nat-Gateway-EIP" {
   
   vpc = true
  tags = {
@@ -166,18 +166,18 @@ resource "aws_eip" "Nat-Gateway-EIP" {
 
 
 # Creating a NAT Gateway
-resource "aws_nat_gateway" "NAT_GATEWAY" {
+resource "aws_nat_gateway" "messaging_NAT_GATEWAY" {
   depends_on = [
-    aws_eip.Nat-Gateway-EIP,
-    aws_internet_gateway.my_igw
+    aws_eip.messaging_Nat-Gateway-EIP,
+    aws_internet_gateway.messaging_igw
   ]
 
 
   # Allocating the Elastic IP to the NAT Gateway
-  allocation_id = aws_eip.Nat-Gateway-EIP.id
+  allocation_id = aws_eip.messaging_Nat-Gateway-EIP.id
   
   # Associating it in the Public Subnet
-  subnet_id = aws_subnet.subnet_a.id
+  subnet_id = aws_subnet.messaging_subnet_a.id
   tags = {
     Name = "Nat-Gateway_Project"
   }
@@ -185,16 +185,16 @@ resource "aws_nat_gateway" "NAT_GATEWAY" {
 
 
 # Creating a Route Table for the Nat Gateway
-resource "aws_route_table" "NAT-Gateway-RT" {
+resource "aws_route_table" "messaging_NAT-Gateway-RT" {
   depends_on = [
-    aws_nat_gateway.NAT_GATEWAY
+    aws_nat_gateway.messaging_NAT_GATEWAY
   ]
 
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.messaging_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.NAT_GATEWAY.id
+    nat_gateway_id = aws_nat_gateway.messaging_NAT_GATEWAY.id
   }
 
   tags = {
@@ -205,17 +205,17 @@ resource "aws_route_table" "NAT-Gateway-RT" {
 
 
 #Create a new Route Table for the Private Subnet
-resource "aws_route_table" "Private-Subnet-RT" {
+resource "aws_route_table" "messaging_Private-Subnet-RT" {
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_nat_gateway.NAT_GATEWAY
+    aws_vpc.messaging_vpc,
+    aws_nat_gateway.messaging_NAT_GATEWAY
   ]
 
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.messaging_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.NAT_GATEWAY.id
+    nat_gateway_id = aws_nat_gateway.messaging_NAT_GATEWAY.id
   }
 
   tags = {
@@ -227,32 +227,32 @@ resource "aws_route_table" "Private-Subnet-RT" {
 # Creating an Route Table Association of the NAT Gateway route 
 # table with the Private Subnet!
 
-resource "aws_route_table_association" "Private-Subnet-RT-Association-a" {
+resource "aws_route_table_association" "messaging_Private-Subnet-RT-Association-a" {
   depends_on = [
-    aws_route_table.Private-Subnet-RT
+    aws_route_table.messaging_Private-Subnet-RT
   ]
 
-  subnet_id      = aws_subnet.private_subnet_a.id
-  route_table_id = aws_route_table.Private-Subnet-RT.id
+  subnet_id      = aws_subnet.messaging_private_subnet_a.id
+  route_table_id = aws_route_table.messaging_Private-Subnet-RT.id
 }
 
 # Creating an Route Table Association of the NAT Gateway route 
 # table with the Private Subnet!
 
-resource "aws_route_table_association" "Private-Subnet-RT-Association-b" {
+resource "aws_route_table_association" "messaging_Private-Subnet-RT-Association-b" {
   depends_on = [
-    aws_route_table.Private-Subnet-RT
+    aws_route_table.messaging_Private-Subnet-RT
   ]
 
-  subnet_id      = aws_subnet.private_subnet_b.id
-  route_table_id = aws_route_table.Private-Subnet-RT.id
+  subnet_id      = aws_subnet.messaging_private_subnet_b.id
+  route_table_id = aws_route_table.messaging_Private-Subnet-RT.id
 }
 
 
 
 #creating an aws iam role for cluster
 resource "aws_iam_role" "eks-iam-role" {
- name = "snappcoins-eks-iam-role"
+ name = "messaging-eks-iam-role"
 
  path = "/"
 
@@ -284,12 +284,12 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EK
 }
 
 #creating a cluster
-resource "aws_eks_cluster" "snappcoins-eks" {
- name = "snappcoins-cluster"
+resource "aws_eks_cluster" "messaging-eks" {
+ name = "messaging-cluster"
  role_arn = aws_iam_role.eks-iam-role.arn
 
  vpc_config {
-  subnet_ids = [aws_subnet.subnet_a.id,aws_subnet.subnet_b.id,aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+  subnet_ids = [aws_subnet.messaging_subnet_a.id,aws_subnet.messaging_subnet_b.id,aws_subnet.messaging_private_subnet_a.id, aws_subnet.messaging_private_subnet_b.id]
  }
 
  depends_on = [
@@ -300,8 +300,8 @@ resource "aws_eks_cluster" "snappcoins-eks" {
 
 
 #creating roles for nodes
-resource "aws_iam_role" "workernodes" {
-  name = "eks-node-group-example"
+resource "aws_iam_role" "messaging-workernodes" {
+  name = "messagging-eks-node-group"
  
   assume_role_policy = jsonencode({
    Statement = [{
@@ -318,34 +318,34 @@ resource "aws_iam_role" "workernodes" {
 #attaching policies to nodes 
  resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role    = aws_iam_role.workernodes.name
+  role    = aws_iam_role.messaging-workernodes.name
  }
  
  resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role    = aws_iam_role.workernodes.name
+  role    = aws_iam_role.messaging-workernodes.name
  }
  
  resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-  role    = aws_iam_role.workernodes.name
+  role    = aws_iam_role.messaging-workernodes.name
  }
  
  resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role    = aws_iam_role.workernodes.name
+  role    = aws_iam_role.messaging-workernodes.name
  }
 
 
 #create a nodegroup
 
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.snappcoins-eks.name
-  node_group_name = "example-node-group"
+resource "aws_eks_node_group" "messaging-nodegroup" {
+  cluster_name    = aws_eks_cluster.messaging-eks.name
+  node_group_name = "messaging-node-group"
 
-  node_role_arn = aws_iam_role.workernodes.arn
+  node_role_arn = aws_iam_role.messaging-workernodes.arn
 
-  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+  subnet_ids = [aws_subnet.messaging_private_subnet_a.id, aws_subnet.messaging_private_subnet_b.id]
 
   scaling_config {
     desired_size = 1
@@ -361,6 +361,6 @@ resource "aws_eks_node_group" "example" {
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_eks_cluster.snappcoins-eks
+    aws_eks_cluster.messaging-eks
   ]
 }
