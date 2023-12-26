b7ce500540306265c878519f454b808476e2f5e9
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-12-26 16:12:18 +0530

    Update VideoPage.js

--- frontend/src/VideoPage.js
+++ frontend/src/VideoPage.js
@@ -16,7 +16,7 @@ function VideoPage() {
 export default VideoPage;
 */
 
-import React from "react";
+/*import React from "react";
 
 function VideoPage() {
   return (
@@ -31,6 +31,28 @@ function VideoPage() {
           src="http://localhost:3009/api/getVideo"
           frameborder="0"
           allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default VideoPage;*/
+import React from "react";
+
+function VideoPage() {
+  return (
+    <div>
+      <h1 className="center-text">Happy Birthday!</h1>
+      <div className="video-wrapper">
+        <iframe
+          width="900"
+          height="500"
+          //src="https://www.veed.io/view/dc7a4afe-7018-4179-be56-b24f7b01740f?panel=share"
+          //src="https://www.veed.io/view/6c156033-53a5-4f6c-ba5f-5485e411b700?panel=share"
+          src="http://localhost:3009/api/getVideo"
+          frameBorder="0"
+          allowFullScreen
         ></iframe>
       </div>
     </div>
