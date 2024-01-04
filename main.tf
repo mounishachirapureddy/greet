d42fca3b0600ee0b666c33cfe7f2d5cd0a831c55
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2024-01-04 10:50:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -353,3 +353,19 @@ resource "null_resource" "install_istio" {
     EOT
   }
 }
+resource "aws_cloudwatch_metric_alarm" "example" {
+  alarm_name          = "example"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "This metric controls instance scale up and down"
+  alarm_actions       = [aws_autoscaling_policy.example.arn]
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.example.name}"
+  }
+}
+
