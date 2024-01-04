a557c93cedc284de5420a03b05997d069d31b9fd
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2024-01-04 15:29:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -369,3 +369,59 @@ resource "aws_cloudwatch_metric_alarm" "example" {
   }
 }
 
+
+# Creating the autoscaling group within us-east-1a availability zone
+resource "aws_autoscaling_group" "mygroup" {
+# Defining the availability Zone in which AWS EC2 instance will be launched
+  availability_zones        = ["ap-south-1a"]
+# Specifying the name of the autoscaling group
+  name                      = "autoscalegroup"
+# Defining the maximum number of AWS EC2 instances while scaling
+  max_size                  = 2
+# Defining the minimum number of AWS EC2 instances while scaling
+  min_size                  = 1
+# Grace period is the time after which AWS EC2 instance comes into service before checking health.
+  health_check_grace_period = 30
+# The Autoscaling will happen based on health of AWS EC2 instance defined in AWS CLoudwatch Alarm 
+  health_check_type         = "EC2"
+# force_delete deletes the Auto Scaling Group without waiting for all instances in the pool to terminate
+  force_delete              = true
+# Defining the termination policy where the oldest instance will be replaced first 
+  termination_policies      = ["OldestInstance"]
+# Scaling group is dependent on autoscaling launch configuration because of AWS EC2 instance configurations
+  launch_configuration      = aws_launch_configuration.aws_autoscale_conf.name
+}
+
+
+# Creating the autoscaling policy of the autoscaling group
+resource "aws_autoscaling_policy" "mygroup_policy" {
+  name                   = "autoscalegroup_policy"
+# The number of instances by which to scale.
+  scaling_adjustment     = 2
+  adjustment_type        = "ChangeInCapacity"
+# The amount of time (seconds) after a scaling completes and the next scaling starts.
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.mygroup.name
+}
+# Creating the AWS CLoudwatch Alarm that will autoscale the AWS EC2 instance based on CPU utilization.
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
+# defining the name of AWS cloudwatch alarm
+  alarm_name = "web_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "2"
+# Defining the metric_name according to which scaling will happen (based on CPU) 
+  metric_name = "CPUUtilization"
+# The namespace for the alarm's associated metric
+  namespace = "AWS/EC2"
+# After AWS Cloudwatch Alarm is triggered, it will wait for 60 seconds and then autoscales
+  period = "60"
+  statistic = "Average"
+# CPU Utilization threshold is set to 10 percent
+  threshold = "10"
+  alarm_actions = [
+        "${aws_autoscaling_policy.mygroup_policy.arn}"
+    ]
+dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.mygroup.name}"
+  }
+}
